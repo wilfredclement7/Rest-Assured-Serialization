8d6c8ac0810514a02e302507bf8520ad90211fbd
wilfredclement7 <42040151+wilfredclement7@users.noreply.github.com>
2018-08-11 01:59:59 +0530

    Update

--- src/test/java/serialization/SerializationTest.java
+++ src/test/java/serialization/SerializationTest.java
@@ -1,25 +1,50 @@
 package serialization;
 
+import java.util.Arrays;
 //import testing.User;
 //import java.util.HashSet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import serialization.Input;
 
-public class SerializationTest {
+public class SerializationTest2 {
  
-    public static void main(String[] args) throws Exception {
-        Input user = new Input();
-        user.setId("0001");
-        user.setType("donut");
-    	user.setName("cake");
-        user.setPpu(0.55);
-        //user.getBatters().getBatter().add(null);
-        
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-         
-        String json = mapper.writeValueAsString(user);
-        System.out.println(json);
+	public static void main(String[] args) throws Exception {
+	    Input user = new Input();
+	    user.setId("0001");
+	    user.setType("donut");
+	    user.setName("Cake");
+	    user.setPpu(0.55);
+
+	    // Add Regular batter
+	    Batter batter1 = new Batter();
+	    batter1.setId("0001");
+	    batter1.setType("Regular");
+
+	    // Add Devil's Food batter
+	    Batter batter2 = new Batter();
+	    batter2.setId("1004");
+	    batter2.setType("Devil's Food");
+
+	    Batters batters = new Batters();
+	    batters.setBatter(Arrays.asList(batter1, batter2));
+	    user.setBatters(batters); 
+	    
+	    // Add toppings
+	    Topping topping1 = new Topping();
+	    topping1.setId("5001");
+	    topping1.setType("Orange");
+
+	    Topping topping2 = new Topping();
+	    topping2.setId("5004");
+	    topping2.setType("Maple");
+
+	    user.setTopping(Arrays.asList(topping1, topping2));
+
+	    ObjectMapper mapper = new ObjectMapper();
+	    mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+	    String json = mapper.writeValueAsString(user);
+	    System.out.println(json); // expected string
     }
 }
